f1b19282a4ea8ebbad8abdfedbad28177c61f191
Daniela Gordas <daniela.gordas@gmail.com>
2020-05-18 18:38:37 +0200

    added tests

---
+++ test/system/questions_test.rb
@@ -0,0 +1,32 @@
+require 'application_system_test_case'
+
+class QuestionsTest < ApplicationSystemTestCase
+  test 'visiting /ask renders the form' do
+    visit ask_url
+    assert_selector 'h1', text: 'Ask your question here!'
+  end
+
+  test 'saying Hello yields a grumpy response from the coach' do
+    visit ask_url
+    fill_in 'question', with: 'Hello'
+    click_on 'Ask'
+
+    assert_text "I don't care, get dressed and go to work!"
+  end
+
+  test 'saying what is your name? yields a grumpy response from the coach' do
+    visit ask_url
+    fill_in 'question', with: 'what is your name?'
+    click_on 'Ask'
+
+    assert_text 'Silly question, get dressed and go to work!'
+  end
+
+  test 'saying I am going to work yields a happy response from the coach' do
+    visit ask_url
+    fill_in 'question', with: 'I am going to work'
+    click_on 'Ask'
+
+    assert_text 'Great!'
+  end
+end
